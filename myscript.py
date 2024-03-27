fe9be1f27772762719a7edaa9bdebcecf24c5c7b
gollorida2023 <gilflorida2023@gmail.com>
2024-03-27 15:18:29 -0500

    add some code to python so i have something to debug

--- myscript.py
+++ myscript.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
-print("Hello, world\n")
+from seleniumwire import webdriver # to be able to use proxy
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+
+# load selenium for brave
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = '/usr/bin/brave-browser'
+browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+
+browser.get('brave://settings/shields')
+
+# load ini file
+# load extensions isten in config. 
